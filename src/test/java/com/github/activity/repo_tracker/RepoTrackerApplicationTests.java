e1594a42cd04b9163683a4cb7b9617a9837bfc89
Satyam Kumar <satyamkumar@Satyams-MacBook-Air.local>
2025-07-26 12:44:07 +0530

    Initial GitHub Repo Tracker Spring Boot project setup

---
+++ src/test/java/com/github/activity/repo_tracker/RepoTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.activity.repo_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepoTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
